ca9c9b7943507fc6c0dcdc3533cfd682f355a711
dinethde <dinethran33@gmail.com>
2026-01-16 21:38:33 +0530

    fix: update authentication context to use new Asgardeo hooks and improve session handling

--- webapps/webapp-template/src/context/AuthContext.tsx
+++ webapps/webapp-template/src/context/AuthContext.tsx
@@ -13,7 +13,7 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-import { SecureApp, useAuthContext } from "@asgardeo/auth-react";
+import { useAsgardeo } from "@asgardeo/react";
 import { useIdleTimer } from "react-idle-timer";
 
 import React, { useContext, useEffect, useState } from "react";
@@ -80,30 +80,30 @@ const AppAuthProvider = (props: { children: React.ReactNode }) => {
   const {
     signIn,
     signOut,
-    getDecodedIDToken,
-    getBasicUserInfo,
-    refreshAccessToken,
-    getIDToken,
-    trySignInSilently,
+    getDecodedIdToken,
+    user,
+    signInSilently,
     getAccessToken,
-    state,
-  } = useAuthContext();
+    isLoading,
+    isSignedIn,
+  } = useAsgardeo();
 
   const setupAuthenticatedUser = async () => {
-    const [userInfo, idToken, decodedIdToken] = await Promise.all([
-      getBasicUserInfo(),
-      getIDToken(),
-      getDecodedIDToken(),
+    const [decodedIdToken, accessToken] = await Promise.all([
+      getDecodedIdToken(),
+      getAccessToken(),
     ]);
 
     dispatch(
       setUserAuthData({
-        userInfo: userInfo,
+        userInfo: user,
         decodedIdToken: decodedIdToken,
       }),
     );
 
-    setTokens(idToken, refreshToken, appSignOut);
+    console.log("decoded id token : ", decodedIdToken);
+
+    setTokens(accessToken, refreshToken, appSignOut);
     await triggerGetUserInfo();
 
     await dispatch(loadPrivileges());
@@ -116,15 +116,15 @@ const AppAuthProvider = (props: { children: React.ReactNode }) => {
       try {
         setAppState(AppState.Loading);
 
-        if (state.isLoading) return;
+        if (isLoading) return;
 
-        if (state.isAuthenticated) {
+        if (isSignedIn) {
           setAppState(AppState.Authenticating);
           await setupAuthenticatedUser();
 
           if (mounted) setAppState(AppState.Authenticated);
         } else {
-          const silentSignInSuccess = await trySignInSilently();
+          const silentSignInSuccess = await signInSilently();
 
           if (mounted)
             setAppState(silentSignInSuccess ? AppState.Authenticating : AppState.Unauthenticated);
@@ -141,16 +141,10 @@ const AppAuthProvider = (props: { children: React.ReactNode }) => {
     return () => {
       mounted = false;
     };
-  }, [state.isAuthenticated, state.isLoading]);
+  }, [isSignedIn, isLoading]);
 
   const refreshToken = async (): Promise<{ accessToken: string }> => {
-    if (state.isAuthenticated) {
-      const accessToken = await getIDToken();
-      return { accessToken };
-    }
-
     try {
-      await refreshAccessToken();
       const accessToken = await getAccessToken();
       return { accessToken };
     } catch (error) {
@@ -179,7 +173,7 @@ const AppAuthProvider = (props: { children: React.ReactNode }) => {
   const renderContent = () => {
     switch (appState) {
       case AppState.Loading:
-        return <PreLoader isLoading message="Loading ..." />;
+        return <PreLoader isLoading message="Authenticating ..." />;
 
       case AppState.Authenticating:
         return <PreLoader isLoading message="Loading User Info ..." />;
@@ -207,9 +201,7 @@ const AppAuthProvider = (props: { children: React.ReactNode }) => {
         appSignOut={appSignOut}
       />
 
-      <SecureApp fallback={<PreLoader isLoading message="We are getting things ready ..." />}>
-        {renderContent()}
-      </SecureApp>
+      {renderContent()}
     </>
   );
 };
@@ -219,3 +211,31 @@ const useAppAuthContext = (): AuthContextType => useContext(AuthContext);
 export { useAppAuthContext };
 
 export default AppAuthProvider;
+
+/*
+
+{
+    "isk": "32a2f8c16ffdefd72a2cd5db4161af3259c30674d0072bb6d119327cd9d368a2",
+    "at_hash": "F-9TqrEMc8_Za-u9jBzKjQ",
+    "sub": "6387963b-c0b4-4268-b77c-b27513d713b3",
+    "amr": [
+        "BasicAuthenticator",
+        "totp"
+    ],
+    "iss": "https://api.asgardeo.io/t/wso2/oauth2/token",
+    "sid": "f78f1157-7f90-4134-8c36-0888b0e484e3",
+    "c_hash": "pENd-U74V0e2ADpLJx-19g",
+    "aud": [
+        "WyZdatcRw0U0QJ4Wl22Wnx_lf7Ua",
+        "choreo:deployment:sandbox"
+    ],
+    "nbf": 1768578931,
+    "azp": "WyZdatcRw0U0QJ4Wl22Wnx_lf7Ua",
+    "org_id": "11737422-7078-8510-1074-820892631194",
+    "org_name": "wso2",
+    "exp": 1768582531,
+    "iat": 1768578931,
+    "jti": "4e06ad2e-6a28-45e1-8d47-70278d8a3e58",
+    "org_handle": "wso2"
+}
+*/
