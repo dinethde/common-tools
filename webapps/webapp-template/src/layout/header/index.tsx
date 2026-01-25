c92a6c581a65886d2c5fd7173d0206e3b6012352
dinethde <dinethran33@gmail.com>
2026-01-25 16:55:44 +0530

    fix: conditionally render BasicBreadcrumbs based on mobile view

--- webapps/webapp-template/src/layout/header/index.tsx
+++ webapps/webapp-template/src/layout/header/index.tsx
@@ -98,7 +98,7 @@ const Header = () => {
           >
             {APP_NAME}
           </Typography>
-          <BasicBreadcrumbs />
+          {!isMobile && <BasicBreadcrumbs />}
         </Box>
 
         <Box sx={{ flexGrow: 0 }}>
