1b9ac013dce6f141b97dc417692d07cf18b00fba
dinethde <dinethran33@gmail.com>
2026-02-05 20:53:37 +0530

    fix: enhance PreLoader component for improved user feedback during authentication

--- webapps/webapp-template/src/context/AuthContext.tsx
+++ webapps/webapp-template/src/context/AuthContext.tsx
@@ -175,10 +175,14 @@ const AppAuthProvider = (props: { children: React.ReactNode }) => {
   const renderContent = () => {
     switch (appState) {
       case AppState.Loading:
-        return <PreLoader isLoading message="Authenticating ..." />;
+        return (
+          <PreLoader hideImage={false} marqueeOn={true} isLoading message="Loading User Info ..." />
+        );
 
       case AppState.Authenticating:
-        return <PreLoader isLoading message="Loading User Info ..." />;
+        return (
+          <PreLoader hideImage={false} marqueeOn={true} isLoading message="Loading User Info ..." />
+        );
 
       case AppState.Authenticated:
         return <AuthContext.Provider value={authContext}>{props.children}</AuthContext.Provider>;
